e077a532f8ad6d34ce08d6ed11a539bf8d9e7769
andywurm <andaiyewurm@gmail.com>
2022-04-09 19:22:49 -0400

    Floating Trash

---
+++ HungrySeaTurtle/Assets/canFloat.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canFloat : MonoBehaviour
+{
+
+    private GameObject can;
+    bool up = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      if(can == null){
+        can = GameObject.Find("can");
+      }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      floatEffect();
+    }
+
+    void floatEffect(){
+
+      var temp = transform.position;
+
+      if(up == true)
+      {
+          temp.y += 0.01f;
+          transform.position=temp;
+
+          if(transform.position.y >= .1f)
+          {
+              up = false;
+          }
+      }
+      if(up == false)
+      {
+          temp.y -= 0.01f;
+          transform.position=temp;
+
+          if(transform.position.y<=-1f)
+          {
+              up = true;
+          }
+      }
+    }
+
+}
