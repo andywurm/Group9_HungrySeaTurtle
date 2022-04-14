d454b38e1fae1932849f2f80ee8bc6cda746855c
Janzheng0816 <47221902+Janzheng0816@users.noreply.github.com>
2022-04-14 04:51:43 -0400

    movement and ground

---
+++ HungrySeaTurtle/Assets/movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+	[SerializeField] Rigidbody2D rigid;
+	[SerializeField] float movements;
+	[SerializeField] float movement2;
+	[SerializeField] int speed;
+	[SerializeField] bool isFacingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+		if (rigid == null)
+            rigid = GetComponent<Rigidbody2D>();
+		speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movements = Input.GetAxis("Horizontal");
+		movement2 = Input.GetAxis("Vertical");
+		
+    }
+	 void FixedUpdate(){
+		rigid.velocity = new Vector2(movements * speed, movement2 * speed);
+		if (movements < 0 && isFacingRight || movements > 0 && !isFacingRight)
+            Flip();
+	 }
+	 void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        isFacingRight = !isFacingRight;
+    }
+}
