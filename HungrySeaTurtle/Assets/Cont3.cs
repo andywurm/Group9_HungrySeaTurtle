8613149f513bd8acf5007b65da65972daf026c41
andywurm <andaiyewurm@gmail.com>
2022-05-14 15:58:23 -0400

    Added Instructions and Level Numbers

--- HungrySeaTurtle/Assets/Cont3.cs
+++ HungrySeaTurtle/Assets/Cont3.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ContInfo2 : MonoBehaviour
+public class Cont3 : MonoBehaviour
 {
   public GameObject DisplayInfo;
-  public GameObject DisplayInfo2;
 
   public void ContinueGame(){
     DisplayInfo.SetActive(false);
-    DisplayInfo2.SetActive(true);
+    Time.timeScale = 1f;
   }
 }
